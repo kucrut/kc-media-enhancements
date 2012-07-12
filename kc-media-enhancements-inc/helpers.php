40cd6f9165877abaff5ab8c6400b935a23cf5065
Dzikri Aziz <kucrut@kucrut.org>
2012-07-12 07:24:13 +0700

    Removed old func

--- kc-media-enhancements-inc/helpers.php
+++ kc-media-enhancements-inc/helpers.php
@@ -1,25 +1,5 @@
 <?php
 
-#if ( !function_exists('kc_get_additional_image_sizes') ) {
-	/**
-	 * Get additional image sizes
-	 *
-	 * @return array Custom image sizes
-	 */
-	 /*
-	function kc_get_additional_image_sizes() {
-		$sizes = array();
-		global $_wp_additional_image_sizes;
-		if ( isset($_wp_additional_image_sizes) && count($_wp_additional_image_sizes) ) {
-			$sizes = apply_filters( 'intermediate_image_sizes', $_wp_additional_image_sizes );
-			$sizes = apply_filters( 'kc_get_additional_image_sizes', $_wp_additional_image_sizes );
-		}
-
-		return $sizes;
-	}
-}
-*/
-
 /*
 function kc_get_public_taxonomies() {
 	$public_taxonomies = array();
